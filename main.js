7ff7efe7c0e2834e1e5a7415c31173ce8e669b7b
Nico-Capy <nicolacorradini97@yahoo.com>
2023-01-19 16:45:35 +0100

    Added a chart

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import Chart from 'chart.js/auto'
 
-setupCounter(document.querySelector('#counter'))
+var placeGraph = document.createElement ("canvas");
+placeGraph.id= "mychart";
+const table = document.getElementById("table1");
+
+const parent = table.parentNode;
+
+new Chart(placeGraph, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+parent.insertBefore(placeGraph,table);
